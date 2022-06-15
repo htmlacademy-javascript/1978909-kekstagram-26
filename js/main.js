8e2ae8c3611d7ad0a0dc0e9ecdea8711db1d294a
Vadim Silagin <silagin0205@gmail.com>
2022-06-16 01:24:30 +0500

    Добавляет вспомогательные функции

--- js/main.js
+++ js/main.js
@@ -1,22 +1,21 @@
-/*
- Функция, возвращающая случайное целое число из переданного диапазона.
- https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
- */
- function getRandomInt(min, max) {
+/*Функция, возвращающая случайное целое число из переданного диапазона.
+ https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random*/
+
+function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
 }
+
 getRandomInt(1, 9);
-/*
-Функция для проверки максимальной длины строки
-https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/length
-*/
+
+/*Функция для проверки максимальной длины строки
+https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/length*/
+
 function checkComment(string, maxSybols) {
-  string = string.length;
-  if (string <= maxSybols) {
+  if (string.length <= maxSybols) {
+    return true;
   }
 }
-string = 'Пример функции';
-maxSybols = 1;
-checkComment(string, maxSybols);
+
+checkComment ('Пример функции', 8);
