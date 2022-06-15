f6971031918b0a38a372e19926ebf83d80e153c2
Vadim Silagin <silagin0205@gmail.com>
2022-06-16 00:08:41 +0500

    Добавляет вспомогательные функции

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
+/*
+ Функция, возвращающая случайное целое число из переданного диапазона.
+ https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+ */
 
+ function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+getRandomInt(1, 9);
+
+/*
+Функция для проверки максимальной длины строки
+https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/length
+*/
+
+
+function checkComment(string, maxSybols) {
+  string = string.length;
+  if (string <= maxSybols) {
+  }
+}
+
+string = 'Пример функции';
+maxSybols = 1;
+checkComment(string, maxSybols);
