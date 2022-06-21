87fb523acb5bb7e1df21aa548374e58b19b48b4f
Keks <keks@htmlacademy.ru>
2022-06-21 17:18:39 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/main.js
@@ -0,0 +1,22 @@
+/*
+Функция, возвращающая случайное целое число из переданного диапазона.
+ https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+ */
+
+function randomInteger(min, max) {
+  const rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+}
+randomInteger(0,8);
+
+/*
+Функция для проверки максимальной длины строки
+https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/length
+*/
+
+function checkComment(string, maxSybols) {
+  if (string.length <= maxSybols) {
+    return true;
+  } return false;
+}
+checkComment('Пример функции',8);
